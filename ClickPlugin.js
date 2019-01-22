cee5d68077e969d74a80524da09c4a4e552632a0
iradynska <46935517+iradynska@users.noreply.github.com>
2019-01-23 00:15:21 +0100

    Create ClickPlugin.js

---
+++ ClickPlugin.js
@@ -0,0 +1,132 @@
+<script>
+
+
+$(function(){
+   $('body').on('click','polygon',function(){ // this is event delegation which will trigger on any path clicked inside the html Body tag. 
+       //  alert('The clicked Kiosk Name or Id  is : '+ $(this).attr('id'));
+       switch($(this).attr('id')) {
+  case '1':
+		   window.open("https://radynska-portfolio.webflow.io/#AboutMe")
+    break;
+  case '2':
+     window.open("https://radynska-portfolio.webflow.io/#Contact")
+    break;
+        case '2':
+        
+         break;
+
+      case '3':
+        
+         break;
+         
+          case '4':
+        
+         break;
+         
+         
+          case '5':
+        
+         break;
+         
+          case '6':
+        
+         break;
+         
+          case '6':
+        
+         break;
+         
+          case '7':
+        
+         break;
+         
+          case '9':
+        
+         break;
+         
+          case '10':
+        
+         break;
+         
+          case '11':
+        
+         break;
+         
+          case '12':
+        
+         break;
+         
+          case '13':
+        
+         break;
+         
+          case '14':
+        
+         break;
+         
+          case '15':
+        
+         break;
+         
+          case '16':
+        
+         break;
+         
+          case '17':
+        
+         break;
+         
+          case '18':
+        
+         break;
+         
+          case '19':
+        
+         break;
+         
+          case '20':
+        
+         break;
+         
+          case '21':
+        
+         break;
+         
+          case '22':
+        
+         break;
+         
+          case '23':
+        
+         break;
+         
+          case '24':
+        
+         break;
+        
+         case '25':
+        
+         break;
+
+ case '26':
+        
+         break;
+
+  default: 
+  		 //alert('The clicked Kiosk Name or Id  is : '+ 'Fuck Off ');
+       window.open("https://radynska-portfolio.webflow.io")
+  
+}
+       
+   });  
+   
+});
+
+$(function(){
+   $('body').on('click','path',function(){ // this is event delegation which will trigger on any path clicked inside the html Body tag. 
+        alert('Fuck It is : '+ $(this).attr('id'));
+       
+   });  
+   
+});
+ </script>
